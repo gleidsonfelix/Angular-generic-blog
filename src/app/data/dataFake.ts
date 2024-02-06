0cc675f1710fd1e2cb46094acc2a16f8f634e5aa
Gleidson Felix <123380999+gleidsonfelix@users.noreply.github.com>
2024-02-06 11:14:43 -0300

    content-create

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,24 @@
 export const dataFake = [
 
     {
-        "id": 1,
-        "photo": "aaa",
-        "title": "Olá"
-    }
+        "id": "1",
+        "contentPhoto": "https://images.pexels.com/photos/4781948/pexels-photo-4781948.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "title": "Carro novo na Área"
+    },
+    {
+        "id": "2",
+        "contentPhoto": "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "title": "Cachorros aleatórios"
+    },
+    {
+        "id": "3",
+        "contentPhoto": "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "title": "Cachorros aleatórios"
+    },
+    {
+        "id": "4",
+        "contentPhoto": "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "title": "Cachorros aleatórios"
+    },
+    
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink,],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
 
-  contentPhoto:string = "https://images.pexels.com/photos/4781951/pexels-photo-4781951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  contentTitle:string = "Olá"
+  contentPhoto:string = ""
+  contentTitle:string = ""
+  private id:string | null = "0"
 
+  constructor(private route:ActivatedRoute) {
+    this.route.paramMap.subscribe((value) => this.id = value.get("id"))
+    
+    this.setValuesToComponenet(this.id)
+  }
 
-  constructor() {}
+  setValuesToComponenet(id:string | null) {
+    const result = dataFake.filter(article => article.id === id)[0]
+    console.log(result.title)
+    this.contentTitle = result.title
+    this.contentPhoto = result.contentPhoto
+  }
+
 }
